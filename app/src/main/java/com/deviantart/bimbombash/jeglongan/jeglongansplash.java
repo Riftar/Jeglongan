b59f0973c2f7ff5762e9d64428ea5413682114c2
Riftar <mukhtarrifqi@gmail.com>
2016-01-02 19:03:23 +0700

    Delet get Berita

--- app/src/main/java/com/deviantart/bimbombash/jeglongan/jeglongansplash.java
+++ app/src/main/java/com/deviantart/bimbombash/jeglongan/jeglongansplash.java
@@ -1,30 +1,37 @@
 package com.deviantart.bimbombash.jeglongan;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 
 public class jeglongansplash extends AppCompatActivity {
 
     private final int SPLASH_DISPLAY_LENGTH = 4000;
+    boolean calibrated;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jeglongansplash);
         ImageView gif = (ImageView) findViewById(R.id.splashScreen);
+        SharedPreferences sharedpreferences = getSharedPreferences("dataCalibration", Context.MODE_PRIVATE);
         gif.setBackgroundResource(R.drawable.jeglongan);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent mainIntent = new Intent(jeglongansplash.this, Calibration.class);
-                jeglongansplash.this.startActivity(mainIntent);
+                decide();
                 jeglongansplash.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+        calibrated = sharedpreferences.getBoolean("Calibrated", false);
     }
 
     @Override
@@ -47,5 +54,22 @@ public class jeglongansplash extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void decide (){
+        //putData();
+        if (calibrated){
+            Intent intent = new Intent(this,MapsActivity.class);
+            startActivity(intent);
+        }
+        if (!calibrated){
+            calibrated = true;
+            SharedPreferences sharedpreferences = getSharedPreferences("dataCalibration", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedpreferences.edit();
+            editor.putBoolean("Calibrated", calibrated);
+            editor.apply();
+            Intent intent = new Intent(this,Calibration.class);
+            startActivity(intent);
+        }
     }
 }
